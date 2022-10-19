80c010fee5a58f4758a148a6f3b2bb65bc6e70fe
FLEXTRA <81846963+FlextraApe@users.noreply.github.com>
2022-10-20 02:59:35 +1000

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import MulticallUpdater from './state/multicall/updater'
 import TransactionUpdater from './state/transactions/updater'
 import App from './App'
 import Providers from './Providers'
-
+ 
 function Updaters() {
   return (
     <>
